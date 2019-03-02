3922b7dd9f33dd31d072d5b6fb2e69dc7ad4dc82
Filip Knyszewski <filip_mateus96@hotmail.com>
2019-03-02 21:09:04 +0100

    final SAT version

--- FINAL_SAT/SAT/SAT.py
+++ FINAL_SAT/SAT/SAT.py
@@ -12,28 +12,18 @@ def solve(heuristic, file):
 
     print("Solving using {} heuristic".format(heuristics[int(heuristic)]))
 
-    #truth_values = sudokus[sdk] # {} when we submit
-    rules = read_files.read_DIMACS_file(file)
-    truth_values = set()
-    to_remove = []
-    for rule in rules:
-        if len(rule) == 1:
-            truth_values.add(int(rule[0]))
-
-    for rule in to_remove:
-        rules.remove(rule)
 
     which_method = int(heuristic)
 
     # List for data analysis EXCEL
     # [#unit_clauses, %of reduction from first simplify, #splits, #backtrackings, #time]
     results = [0, 0, 100., 0, 0, 0]
-    results[1] = len(truth_values)
     split_count = 0
     back_track_count = 0
     problem_start_time = time.time()
     rules = read_files.read_DIMACS_file("sdks/sudoku-rules.txt")
-    rules, literals_dict, temp = read_files.init_database(rules)
+    rules, literals_dict, truth_values = read_files.init_database(rules)
+    results[1] = len(truth_values)
     old_clauses_count = len(rules)
     done = False
     rules_before_split, literals_dict_before_split, truth_values_before_split = {}, {}, {}
@@ -61,6 +51,10 @@ def solve(heuristic, file):
             print("Solved in {0:.2f}s".format(time.time() - problem_start_time))
             print("# splits {}".format(split_count))
             done = True
+
+            with open("{}.out".format(file),'w') as f:
+                for truth in truth_values:
+                    f.write("{} 0\n".format(truth))
 
 
         elif old_clauses_count == new_clauses_count and back_track == False:
